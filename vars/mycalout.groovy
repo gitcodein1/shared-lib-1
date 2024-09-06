49eb55f755b4bc4688bc3f919fcd0828708f7d35
gitcodein1 <145888378+gitcodein1@users.noreply.github.com>
2024-09-06 17:06:44 +0530

    Update mycalout.groovy

--- vars/mycalout.groovy
+++ vars/mycalout.groovy
@@ -1,6 +1,11 @@
 import com.cal.*;
-def call (a,b) {
+def addfunc (a,b) {
   def calculator = new mycal()
   def result = calculator.add(a, b)
   echo "${a} + ${b} = ${result}"
+}
+def mulfunc (a,b) {
+  def calculator = new mycal()
+  def result = calculator.mul(a, b)
+  echo "${a} X ${b} = ${result}"
 }
