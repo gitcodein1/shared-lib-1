62f21a553280e1ed20cd58b43b80db91b1e6f531
gitcodein1 <145888378+gitcodein1@users.noreply.github.com>
2024-09-07 01:51:10 +0530

    Update and rename mycalout.groovy to myCalOut.groovy

--- vars/myCalOut.groovy
+++ vars/myCalOut.groovy
@@ -1,11 +1,11 @@
 import com.cal.*;
 def addfunc (a,b) {
   def calculator = new mycal()
-  def result = calculator.add(a, b)
+  def result = calculator.add(a,b)
   echo "${a} + ${b} = ${result}"
 }
 def mulfunc (a,b) {
   def calculator = new mycal()
-  def result = calculator.mul(a, b)
+  def result = calculator.mul(a,b)
   echo "${a} X ${b} = ${result}"
 }
